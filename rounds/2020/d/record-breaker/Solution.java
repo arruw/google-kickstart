e34d02a295d8cd24e53a51957736d8b6cf77e003
Matjaž Mav <matjaz.mav@gmail.com>
2020-07-12 07:26:35 +0200

    2020-d-a-record-breaker

---
+++ rounds/2020/d/record-breaker/Solution.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+
+    private static final Scanner _in = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+
+    private static int solve() {
+        final int N = _in.nextInt();
+        _in.nextLine();
+        // final int[] V = Arrays.stream(_in.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int[] V = new int[N+2];
+        V[0] = -1;
+        for (int i = 0; i < N; i++) {
+            V[i+1] = _in.nextInt();
+        }
+        V[N+1] = -1;
+
+        int max = -1;
+        int solution = 0;
+        int prev = -1, curr;
+        boolean asc = true;
+        for (int i = 0; i < N+2; i++) {
+            curr = V[i];
+            if (asc && curr < prev && max < prev) {
+                solution++;
+                max = prev;
+            }
+            asc = curr > prev;
+            prev = curr;
+        }
+
+        return solution;
+    }
+
+    public static void main(String[] args) {
+        int T = _in.nextInt();
+        for (int i = 1; i <= T; ++i) {
+            System.out.println("Case #" + i + ": " + solve());
+        }
+    }
+}
